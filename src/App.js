0ad2ee0c809849d1f438ef0f640ca822a7bf4aa4
p-cap <64616459+p-cap@users.noreply.github.com>
2020-06-19 13:01:23 -0700

    Disabled CORS/Updated App.js on mock-gmail

--- src/App.js
+++ src/App.js
@@ -3,27 +3,40 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
-
-  state = {
-    emails: ''
+  constructor(props){
+    super(props)
+    this.state = {
+    emails: []
   }
-
+}
   async componentDidMount() {
-    fetch('http://localhost:3001/emails')
-    .then(response => response.json())
-    .then(json => this.setState ({emails: json[1].sender}))
-  }
+      fetch('http://localhost:3001/emails')
+      .then(response => response.json())
+      .then(json => this.setState ({emails: json}))
+    }
 
   render() {
-    return (
-      <ul>
-      this still works!
-      sitll works!
-       <li>{this.state.emails}</li>
-      </ul>
-    )
+    const senders = this.state.emails.map(senders => <li>{senders.sender}</li>)
+    const subjects = this.state.emails.map(subjects => <li>{subjects.subject}</li>)
+    return (      
+      <div>
+        <table>
+          <tr>
+            <th>Subject Line</th>
+            <th>Sender</th>
+          </tr>
+          <tr>
+            <td>{subjects}</td>
+            <td>{senders}</td>
+          </tr>
+        </table>
+      </div>
+    ) 
   }
 
 }
-
 export default App;
+
+
+ //const options = this.state.try.map((item, index) => <li key={index}>{`${item.test} in ${item.test1}`}</li>)
+ 
